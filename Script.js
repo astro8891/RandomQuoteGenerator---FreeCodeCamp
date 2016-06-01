584067d14463bb8e31607e9a2fc947db51084504
astro8891 <ashley.j.stewart@live.com>
2016-06-01 23:37:17 +1200

    js not working, debugging

--- Script.js
+++ Script.js
@@ -1,8 +1,10 @@
-$(function() {
+$(document).ready(function() {
+    console.log("ready!");
 
     var quotes = ["Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away —Antoine de Saint-Exupéry", "You miss 100 percent of the shots you never take —Wayne Gretzky", "You must be the change you wish to see in the world —Gandhi", "When hungry, eat your rice; when tired, close your eyes. Fools may laugh at me, but wise men will know what I mean —Lin-Chi"]
 
-    $("btn").click(function() {
-        console.log("Test")
-    })
+    $("QuoteButton").click(function() {
+        alert("Test");
+    });
+
 });
